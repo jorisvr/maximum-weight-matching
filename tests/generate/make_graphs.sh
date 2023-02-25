ce5ff2d5812fd4df22ed7d7c7692dbec735215da
Joris van Rantwijk <joris@jorisvr.nl>
2023-02-25 16:50:36 +0100

    Add test graphs

--- tests/generate/make_graphs.sh
+++ tests/generate/make_graphs.sh
@@ -7,60 +7,39 @@ set -e
 
 SRCDIR="$(dirname $0)"
 
-# Usage: gen_random SEED N M
-gen_random() {
-    python3 $SRCDIR/make_random_graph.py --seed $1 $2 $3 \
-      > graph_rnd_s${1}_n${2}_m${3}.gr
+make_random_graph() {
+    python3 $SRCDIR/make_random_graph.py "$@"
 }
 
-# Usage: gen_slow STRUCTURE N M
-gen_slow() {
-    python3 $SRCDIR/make_slow_graph.py --structure $1 $2 \
-      > graph_slow_${1}_n${2}_m${3}.gr
+make_slow_graph() {
+    python3 $SRCDIR/make_slow_graph.py "$@"
 }
 
+triangles() {
+    python3 $SRCDIR/triangles.py "$@"
+}
 
-gen_random 101 250 31125
-gen_random 102 250 31125
-gen_random 103 250  3952
-gen_random 104 250  3952
-gen_random 105 250  2500
-gen_random 106 250  2500
+make_random_graph --seed 101 1000 10000 > random_n1000_m10000.edge
+make_random_graph --seed 102 2000 10000 > random_n2000_m10000.edge
+make_random_graph --seed 103 4000 10000 > random_n4000_m10000.edge
 
-gen_random 111 500 124750
-gen_random 112 500 124750
-gen_random 113 500  11180
-gen_random 114 500  11180
-gen_random 115 500   5000
-gen_random 116 500   5000
+make_slow_graph chain 1000 > chain_n1000.edge
+make_slow_graph chain 5000 > chain_n5000.edge
+make_slow_graph chain 10000 > chain_n10000.edge
+make_slow_graph chain 20000 > chain_n20000.edge
+make_slow_graph chain 50000 > chain_n50000.edge
+make_slow_graph chain 100000 > chain_n100000.edge
 
-gen_random 121 1000 499500
-gen_random 122 1000 499500
-gen_random 123 1000  31622
-gen_random 124 1000  31622
-gen_random 125 1000  10000
-gen_random 126 1000  10000
+make_slow_graph sparse 1004 > sparse_delta_n1004.edge
+make_slow_graph sparse 2004 > sparse_delta_n2004.edge
+make_slow_graph sparse 5004 > sparse_delta_n5004.edge
+make_slow_graph sparse 10004 > sparse_delta_n10004.edge
+make_slow_graph sparse 20004 > sparse_delta_n20004.edge
+make_slow_graph sparse 40004 > sparse_delta_n40004.edge
 
-gen_random 133 2000   89442
-gen_random 134 2000   89442
-gen_random 135 2000   20000
-gen_random 136 2000   20000
-
-gen_random 143 5000  353553
-gen_random 144 5000  353553
-gen_random 145 5000   50000
-gen_random 146 5000   50000
-
-gen_random 155 10000 100000
-gen_random 156 10000 100000
-
-gen_slow dense    252  4095
-gen_slow dense    500 15875
-gen_slow dense   1000 63000
-gen_slow sparse   252   312
-gen_slow sparse   500   622
-gen_slow sparse  1004  1252
-gen_slow sparse  2004  2502
-gen_slow sparse  5004  6252
-gen_slow sparse 10004 12502
+triangles 334 1 > triangles_n1002.edge
+triangles 1667 1 > triangles_n5001.edge
+triangles 3334 1 > triangles_n10002.edge
+triangles 16667 1 > triangles_n50001.edge
+triangles 33334 1 > triangles_n100002.edge
 
